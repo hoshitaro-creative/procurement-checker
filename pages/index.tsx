8601be2c26d89da2c8cbc799b058be2c18f430fa
viewpointics-tan <viewpointicstan@gmail.com>
2022-04-07 15:43:45 +0900

    update: firestoreを読み込み表示できるようにした

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,32 @@
-import Layout from '../components/Layout'
+import {
+  DocumentData,
+  QueryDocumentSnapshot,
+  collection,
+  getDocs,
+  getFirestore,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { Flex } from "@chakra-ui/react";
+import Layout from "../components/Layout";
 
-const IndexPage = () => (
-  <Layout title="トップページ_調達チェッカー">
-    <h1>更新情報</h1>
-  </Layout>
-)
+const IndexPage = () => {
+  const [docs, setDocs] = useState<QueryDocumentSnapshot<DocumentData>[]>([]);
+  useEffect(() => {
+    const db = getFirestore();
+    getDocs(collection(db, "procurement")).then((snapshot) => {
+      setDocs(snapshot.docs);
+    });
+  }, []);
+  return (
+    <Layout title="トップページ_調達チェッカー">
+      <h1>更新情報</h1>
+      <Flex>
+        {docs.map((doc, i) => (
+          <div key={i}>{doc.ref.path}</div>
+        ))}
+      </Flex>
+    </Layout>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
