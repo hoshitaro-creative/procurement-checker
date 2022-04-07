aa64f21d09a379d722234171af30095d562bb299
viewpointics-tan <viewpointicstan@gmail.com>
2022-04-07 16:55:09 +0900

    update: スクレイピングの結果をfirestoreに書き込むようにした

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,40 +1,52 @@
 import * as functions from "firebase-functions";
 import * as playwright from "playwright-aws-lambda";
 
-const query = "教育";
-
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const procurement = functions.https.onRequest(
-  async (request, response) => {
-    // functions.logger.info("Hello logs!", { structuredData: true });
-    // response.send("Hello from Firebase!");
-    const browser = await playwright.launchChromium({ headless: true });
-    const page = await browser.newPage();
-    await page.goto("https://www.p-portal.go.jp/pps-web-biz/UAA01/OAA0101", {
-      waitUntil: "networkidle",
+export const procurement = functions.pubsub.schedule(
+  "every 3 hours synchronized",
+).onRun(
+  async () => {
+    const query = await firestore().collection("procurement").get();
+    const docNames = query.docs.map((doc) => {
+      const path = doc.ref.path;
+      const docName = path.split("/")[1];
+      return docName;
     });
-    await page.locator("#case-name").type(`${query}`);
-    await page.locator("#OAA0102").click();
-    await page.locator("#tri_WAA0101FM01 > div.result-detail > table > tbody")
-      .waitFor({
-        state: "attached",
+
+    const browser = await playwright.launchChromium({ headless: true });
+    const fetchData = async (docName: string) => {
+      const page = await browser.newPage();
+      await page.goto("https://www.p-portal.go.jp/pps-web-biz/UAA01/OAA0101", {
+        waitUntil: "networkidle",
       });
-    const titles = await page.locator(
-      "div.result-detail > table > tbody > tr > td:nth-child(2)",
-    )
-      .evaluateAll(
-        (tds) => tds.map((td) => td.textContent),
-      );
-    const ids = await page.locator(
-      "div.result-detail > table > tbody > tr > td:nth-child(1)",
-    )
-      .evaluateAll(
-        (tds) => tds.map((td) => td.textContent),
-      );
+      await page.locator("#case-name").type(`${docName}`);
+      await page.locator("#OAA0102").click();
+      await page.locator("#tri_WAA0101FM01 > div.result-detail > table > tbody")
+        .waitFor({
+          state: "attached",
+        });
+      const titles = await page.locator(
+        "div.result-detail > table > tbody > tr > td:nth-child(2)",
+      )
+        .evaluateAll(
+          (tds) => tds.map((td) => td.textContent),
+        );
+      const ids = await page.locator(
+        "div.result-detail > table > tbody > tr > td:nth-child(1)",
+      )
+        .evaluateAll(
+          (tds) => tds.map((td) => td.textContent),
+        );
+      await page.close();
+      await firestore().collection("procurement").doc(docName).set({
+        ids,
+        titles,
+      });
+    };
     await browser.close();
-    response.json([ids, titles]);
+    docNames.map(async (docName) => await fetchData(docName));
   },
 );
 
@@ -42,6 +54,7 @@ import * as admin from "firebase-admin";
 admin.initializeApp();
 
 import allowedEmails from "./allowedEmails";
+import { firestore } from "firebase-admin";
 
 exports.allowUserByEmail = functions.auth.user().onCreate((user) => {
   const uid = user.uid;
